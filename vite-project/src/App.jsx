deb28bf18a432f483e94e622bbae8eba1a19ab0a
Mohd Umar <ua1411978@gmail.com>
2023-05-07 19:43:14 +0530

    we move 30 min ahead and create detail page and show data based on id through Vans Components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,8 @@ import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Vans from './pages/Vans'
+import VanDetails from './pages/VanDetails'
+
 function App() {
  
   return (
@@ -17,6 +19,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/vans' element={<Vans/>}/>
+        <Route path='/vans/:id' element={<VanDetails/>}/>
       </Routes>
     </BrowserRouter>
   )
--- vite-project/src/pages/Vans.jsx
+++ vite-project/src/pages/Vans.jsx
@@ -1,26 +1,28 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-function Vans(){
-    const [vans,setVans]=useState([]);
-    useEffect(()=>{
-        fetch("api/vans")
-            .then(res=>res.json())
-            .then(data=>setVans(data.vans))
-    },[]);
-    const vanElements = vans.map(van=>(
-        <div className="van-til" key={van.id}>
-            <img src={van.imageUrl} alt="" />
-            <div className="van-info">
-                <h3>{van.name}</h3>
-                <p>${van.price} <span>/day</span> </p>
-            </div>
-            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+function Vans() {
+  const [vans, setVans] = useState([]);
+  useEffect(() => {
+    fetch("api/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+  const vanElements = vans.map((van) => (
+    <div className="van-til" key={van.id}>
+        {/* Here we send data or we render component based on specific id */}
+      <Link to={`/vans/${van.id}`}>
+        <img src={van.imageUrl} alt="" />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price} <span>/day</span>{" "}
+          </p>
         </div>
-    ))
-    return(
-        <>
-            {vanElements}
-        </>
-    );
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+      </Link>
+    </div>
+  ));
+  return <>{vanElements}</>;
 }
-export default Vans
+export default Vans;
